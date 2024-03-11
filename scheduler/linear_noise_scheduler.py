751cf952702528c822c04d1c349ac456d4bb2549
explainingai-code <explainingai.official@gmail.com>
2024-03-11 16:05:11 +0530

    Fix for https://github.com/explainingai-code/DDPM-Pytorch/issues/5
    1. Moving alphas to same device as input before indexing with timesteps
    2. Returning mean,x0 for t=0 instead of incorrectly returning mean,mean

--- scheduler/linear_noise_scheduler.py
+++ scheduler/linear_noise_scheduler.py
@@ -27,15 +27,15 @@ class LinearNoiseScheduler:
         original_shape = original.shape
         batch_size = original_shape[0]
         
-        sqrt_alpha_cum_prod = self.sqrt_alpha_cum_prod[t].reshape(batch_size)
-        sqrt_one_minus_alpha_cum_prod = self.sqrt_one_minus_alpha_cum_prod[t].reshape(batch_size)
+        sqrt_alpha_cum_prod = self.sqrt_alpha_cum_prod.to(original.device)[t].reshape(batch_size)
+        sqrt_one_minus_alpha_cum_prod = self.sqrt_one_minus_alpha_cum_prod.to(original.device)[t].reshape(batch_size)
         
         # Reshape till (B,) becomes (B,1,1,1) if image is (B,C,H,W)
-        for _ in range(len(original_shape)-1):
+        for _ in range(len(original_shape) - 1):
             sqrt_alpha_cum_prod = sqrt_alpha_cum_prod.unsqueeze(-1)
-        for _ in range(len(original_shape)-1):
+        for _ in range(len(original_shape) - 1):
             sqrt_one_minus_alpha_cum_prod = sqrt_one_minus_alpha_cum_prod.unsqueeze(-1)
-
+        
         # Apply and Return Forward process equation
         return (sqrt_alpha_cum_prod.to(original.device) * original
                 + sqrt_one_minus_alpha_cum_prod.to(original.device) * noise)
@@ -43,23 +43,24 @@ class LinearNoiseScheduler:
     def sample_prev_timestep(self, xt, noise_pred, t):
         r"""
             Use the noise prediction by model to get
-            xt-1 using xt and the nosie predicted
+            xt-1 using xt and the noise predicted
         :param xt: current timestep sample
         :param noise_pred: model noise prediction
         :param t: current timestep we are at
         :return:
         """
-        x0 = (xt - (self.sqrt_one_minus_alpha_cum_prod[t] * noise_pred)) / torch.sqrt(self.alpha_cum_prod[t])
+        x0 = ((xt - (self.sqrt_one_minus_alpha_cum_prod.to(xt.device)[t] * noise_pred)) /
+              torch.sqrt(self.alpha_cum_prod.to(xt.device)[t]))
         x0 = torch.clamp(x0, -1., 1.)
-
-        mean = xt - ((self.betas[t])*noise_pred)/(self.sqrt_one_minus_alpha_cum_prod[t])
-        mean = mean / torch.sqrt(self.alphas[t])
+        
+        mean = xt - ((self.betas.to(xt.device)[t]) * noise_pred) / (self.sqrt_one_minus_alpha_cum_prod.to(xt.device)[t])
+        mean = mean / torch.sqrt(self.alphas.to(xt.device)[t])
         
         if t == 0:
-            return mean, mean
+            return mean, x0
         else:
-            variance = (1-self.alpha_cum_prod[t-1]) / (1.0 - self.alpha_cum_prod[t])
-            variance = variance * self.betas[t]
+            variance = (1 - self.alpha_cum_prod.to(xt.device)[t - 1]) / (1.0 - self.alpha_cum_prod.to(xt.device)[t])
+            variance = variance * self.betas.to(xt.device)[t]
             sigma = variance ** 0.5
             z = torch.randn(xt.shape).to(xt.device)
             
@@ -67,4 +68,4 @@ class LinearNoiseScheduler:
             # variance = self.betas[t]
             # sigma = variance ** 0.5
             # z = torch.randn(xt.shape).to(xt.device)
-            return mean + sigma*z, x0
+            return mean + sigma * z, x0
